e36b6e8bd63c46084b01faecf18bb02c54d773b3
25BCSH95-Tanmaya-Kumar-Pradhan <ultraaeonus@gmail.com>
2026-06-04 15:06:32 +0530

    Update First_Non_Repeating_Character.java

--- First_Non_Repeating_Character.java
+++ First_Non_Repeating_Character.java
@@ -12,3 +12,30 @@ Hint:
 
 You need frequency logic using loops.
 */
+import java.util.Scanner;
+
+public class First_non_repeating {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the string:");
+			String string = sc.nextLine();
+			for(int i=0;i<string.length();i++)
+			{
+				for(int j=i+1;j<string.length();j++)
+				{
+					if(string.charAt(i)==string.charAt(j))
+					{
+						System.out.println("First repeating character: "+string.charAt(i));
+						return;
+					}
+				}
+			}
+			System.out.println("There were no repeating characters in the given string");
+			sc.close();
+		
+	}
+
+}
