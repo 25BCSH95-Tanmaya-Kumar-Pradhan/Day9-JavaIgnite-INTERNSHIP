b528c5f5ce6e14097baa97095e3d40495da28a7a
25BCSH95-Tanmaya-Kumar-Pradhan <ultraaeonus@gmail.com>
2026-06-04 14:58:18 +0530

    Update EmailValidator.java

--- EmailValidator.java
+++ EmailValidator.java
@@ -13,3 +13,17 @@ Output:
 JAVA
 java
 */
+import java.util.Scanner;
+public class Email_validator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the string:");
+		String string = sc.nextLine();
+		System.out.println("Upper case: "+string.toUpperCase());
+		System.out.println("Lower case: "+string.toLowerCase());
+		sc.close();
+	}
+
+}
